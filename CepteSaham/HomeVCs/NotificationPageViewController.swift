98e9edbcf8854018f256b387e6a7d1bf0e2afe53
Berkay Demir <berkayydmir@gmail.com>
2024-08-01 13:50:15 +0300

    feat : notification page added

---
+++ CepteSaham/HomeVCs/NotificationPageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotificationPageViewController.swift
+//  CepteSaham
+//
+//  Created by Berkay Demir on 1.08.2024.
+//
+
+import UIKit
+
+class NotificationPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
