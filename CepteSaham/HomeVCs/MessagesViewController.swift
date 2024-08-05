c9edfee486183e2cee4cdcec458823201f3f54ab
Berkay Demir <berkayydmir@gmail.com>
2024-08-05 13:20:01 +0300

    feat : collectionview deleted from message vc

--- CepteSaham/HomeVCs/MessagesViewController.swift
+++ CepteSaham/HomeVCs/MessagesViewController.swift
@@ -1,30 +1,10 @@
 import UIKit
 
-class MessagesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class MessagesViewController: UIViewController {
     
-    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView.dataSource = self
-        collectionView.delegate = self
     }
-    
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ActiveUserCell", for: indexPath) as? ActiveUserCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        cell.imageView.image = UIImage(named: "rp-6")
-        return cell
-    }
-    
-    /*func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 30, height: 30)
-    }*/
+
 }
